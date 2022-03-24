11ec86086667964f9df7428f55bbd1e743a976b3
Sumedh Bala <sumedhbala@gmail.com>
2022-03-24 15:51:35 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            <Menu />
+            <Menu dishes={this.state.dishes} />
           </div>
         </Navbar>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
